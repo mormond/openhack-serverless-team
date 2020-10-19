38dbc1d33fd4381dc943f03482fdc43c7bf602b2
Mike Ormond <mormond@microsoft.com>
2020-10-19 17:02:23 +0000

    Python EventHubs

--- python/ProcessPosEvents/__init__.py
+++ python/ProcessPosEvents/__init__.py
@@ -4,7 +4,10 @@ import logging
 import azure.functions as func
 
 
-def main(events: List[func.EventHubEvent]):
+def main(events: List[func.EventHubEvent], doc: func.Out[func.Document]):
     for event in events:
-        logging.info('Python EventHub trigger processed an event: %s',
-                        event.get_body().decode('utf-8'))
+
+        payload = event.get_body().decode('utf-8')
+
+        logging.info('Python EventHub trigger processed an event: %s', payload)
+        doc.set(func.Document.from_json(payload))
